9ef0c1b8fe849cd2c4ef85955cd56a19cb5c1088
Mugrebot <mikeongay@icloud.com>
2023-05-04 18:42:16 -0400

    vercel cleanup

--- packages/nextjs/pages/rounds.tsx
+++ packages/nextjs/pages/rounds.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useRouter } from 'next/router';
 import { useDeployedContractInfo, useScaffoldContractRead } from '~~/hooks/scaffold-eth';
 import { Abi } from "abitype";
